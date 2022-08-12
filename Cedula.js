4e0e8b7cc16bf2e81bd89af752983875ddb1c5b5
NicolasZapata17 <zapataacostanicolasalejandro@gmail.com>
2022-08-12 11:29:39 -0500

    quitando JS

--- Cedula.js
+++
@@ -1,8 +0,0 @@
-var doc = 1234
-const resultado = "Su genero es:"
-
-if(doc>111111){
-    console.log(resultado + "Femenino");
-}else {
-    console.log(resultado + "Masculino")
-}
--- Valor-Absoluto.js
+++
@@ -1,8 +0,0 @@
-var x = 53;
-const respuesta = "El valor Absoluto es:";
-
-if (x>0){
-    console.log(respuesta + x);
-}else{
-    console.log(respuesta + x*-1);
-}
